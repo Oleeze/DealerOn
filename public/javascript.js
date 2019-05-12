7b9f2ef196b88240e7e1414af8e15541b99db0f3
Oleg Rudenko <orudenkony@gmail.com>
2019-05-12 10:24:14 -0700

    Creating Dom with requested data

--- public/javascript.js
+++ public/javascript.js
@@ -1,7 +1,7 @@
 function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
+    xmlHttp.onreadystatechange = () => { 
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
             callback(xmlHttp.responseText);
     }
@@ -10,5 +10,21 @@ function httpGetAsync(theUrl, callback)
 }
 
 httpGetAsync("/data", (resp) => {
-  console.log(resp)
+  JSON.parse(resp).map((person, index) => {
+    let section = document.createElement("div");
+    let quote = document.createElement("h2");
+    let birthday = document.createElement("h4");
+    let photo = document.createElement("img");
+    
+    section.className="section";
+    quote.innerHTML = `${person.quote} -${person.name}`
+    birthday.innerHTML = `${person.name} was born on ${person.birthday}`
+    photo.src = `${person.image_url}`
+
+    document.body.appendChild(section);
+    document.getElementsByClassName("section")[index].appendChild(photo);
+    document.getElementsByClassName("section")[index].appendChild(quote);
+    document.getElementsByClassName("section")[index].appendChild(birthday);
+
+  })
 })
